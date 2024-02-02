b3bf0a969dd02c19d458a421a46f8adca5013748
Ben Guaragno <scouterben@gmail.com>
2024-02-02 12:59:13 -0700

    Update config.py to handle a missing .config

--- config.py
+++ config.py
@@ -3,15 +3,16 @@ import json
 CONFIG_FILE="/home/ben/python/.config"
 
 def read(k=None):
-    with open(CONFIG_FILE, 'r') as f:
-        json_str=f.readline()
-        try:
+    try:
+        with open(CONFIG_FILE, 'r') as f:
+            json_str=f.readline()
             config=json.loads(json_str)
-        except:
-            config={}
 
-        if isinstance(k,str) and k not in config.keys():
-            config[k]='F'
+            if isinstance(k,str) and k not in config.keys():
+                config[k]='F'
+            return config
+    except:
+        config={}
         return config
 
 def write(d):
